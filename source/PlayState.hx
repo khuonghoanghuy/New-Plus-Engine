ecf4eabe1380d4722b0dd128932ade1526827e14
BambiGaming2022 <116512242+BambiGaming2022@users.noreply.github.com>
2023-07-29 19:08:16 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1192,7 +1192,7 @@ class PlayState extends MusicBeatState
 		doof.cameras = [camHUD];
 
 		// new plus engine
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "New Plus Engine v" + VERSION_PLUSENGINE, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Plus Engine v1.0", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
